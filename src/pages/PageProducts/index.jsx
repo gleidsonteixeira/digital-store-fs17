a04d82cc3e7e5defffb468686db53e792e85e3bf
Francisco Gleidson Teixeira <03538064393@DIGITALCOLLEGE.LOCAL>
2024-01-08 08:09:40 -0300

    initial

---
+++ src/pages/PageProducts/index.jsx
@@ -0,0 +1,22 @@
+import { styled } from "styled-components";
+
+import { Button } from 'primereact/button';
+import { RadioButton } from 'primereact/radiobutton';
+import { InputText } from 'primereact/inputtext';
+        
+        
+
+const PageProductsContainer = styled.div`
+    padding: 100px;
+`;
+
+const PageProducts = () => {
+    return (
+        <PageProductsContainer>
+            
+            
+        </PageProductsContainer>
+    );
+}
+
+export default PageProducts;
